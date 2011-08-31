0c303b4dc2f4f362e116ea2ac7e76b0e4c263984
Kenta Sato <karupa@cpan.org>
2011-09-01 08:55:00 +0900

    MySQLのTriggerでコメント投稿日時を入れてあげる＆それを見て取ってくるように。

--- webapp/perl/lib/Isucon.pm
+++ webapp/perl/lib/Isucon.pm
@@ -11,11 +11,12 @@ our $VERSION = 0.01;
 
 sub load_config {
     my $self = shift;
-    return $self->{_config} if $self->{_config};
-    open( my $fh, '<', $self->root_dir . '/../config/hosts.json') or die $!;
-    local $/;
-    my $json = <$fh>;
-    $self->{_config} = decode_json($json);    
+    return $self->{_config} ||= do {
+        open( my $fh, '<', $self->root_dir . '/../config/hosts.json') or die $!;
+        local $/;
+        my $json = <$fh>;
+        decode_json($json);
+    };
 }
 
 sub dbh {
@@ -36,8 +37,7 @@ filter 'recent_commented_articles' => sub {
     sub {
         my ( $self, $c )  = @_;
         $c->stash->{recent_commented_articles} = $self->dbh->selectall_arrayref(
-            'SELECT a.id, a.title FROM comment c INNER JOIN article a ON c.article = a.id 
-            GROUP BY a.id ORDER BY MAX(c.created_at) DESC LIMIT 10',
+            'SELECT id, title FROM article ORDER BY commented_at DESC LIMIT 10',
             { Slice => {} });
         $app->($self,$c);
     }
